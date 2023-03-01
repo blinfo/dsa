8d62f863d6aac267a1f43e0f833b37431fee6f27
hakan <hakan@NL579>
2023-03-01 14:24:24 +0100

    DNN-172 Jenkinsfil

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('Building application and deploy to artifactory') {
+            agent {
+                docker {
+                    image 'docker.repos.blinfo.se/blinfo/maven:3.8.5-jdk-17'
+                    args '-e MAVEN_CONFIG=/home/jenkins/.m2'
+                }
+            }
+            steps {
+                dir("dsa"){
+                    sh "mvn clean install -U"
+                }
+                dir ('dsa/dsa-domain'){
+                    sh 'mvn deploy -DskipTests'
+                }
+            }
+        }
+    }  
+}
